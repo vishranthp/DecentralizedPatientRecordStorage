66391725c6663e1997968ce21a24a515694ba3af
vishranthp <vishranthp@gmail.com>
2023-03-04 14:56:55 -0600

    Changes to Patient and Doctor contracts

---
+++ contracts/Account.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract HealthTokenAccount is IERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20Votes
+{
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PATIENT_ROLE = keccak256("PATIENT_ROLE");
+
+    constructor() ERC20("PatientToken", "DPRS") ERC20Permit("PatientToken") {
+        _mint(msg.sender, 100000000 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(PATIENT_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
--- contracts/DPRPatient.sol
+++ contracts/DPRPatient.sol
@@ -1,21 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./Doctor.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { DPRDoctor } from "./DPRDoctor.sol";
+import "./Account.sol";
 
-contract DPRPatient is IERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20Votes
+contract DPRPatient
 {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PATIENT_ROLE = keccak256("PATIENT_ROLE");
-
-    mapping(address => Patient) Patients;
+    mapping(address => Patient) Patients;    
     mapping(address => mapping(uint => PatientDesease)) Diseases;
     mapping(address => mapping(uint8 => mapping(uint8 => Medicene))) Medicenes;
+    mapping(address => mapping(uint => mapping(address => Agreement))) Agreements;
 
     struct Patient{
         uint8 Age;
@@ -40,50 +34,22 @@ contract DPRPatient is IERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20V
         string Concentration;
     }
 
-    constructor() ERC20("PatientToken", "DPRS") ERC20Permit("PatientToken") {
-        _mint(msg.sender, 100000000 * 10 ** decimals());
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-        _grantRole(PATIENT_ROLE, msg.sender);
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._burn(account, amount);
+    struct Agreement {
+        uint TokenTransferred;
     }
 
     // Add a Patient
-    function addPatient(uint8 age, string memory firstName, string memory lastName) public
+    function addPatient(uint8 age, string memory firstName, string memory lastName, IERC20 token) public
     {
         Patient storage patient = Patients[msg.sender];
         patient.Age = age;
         patient.LastName = lastName;
         patient.FirstName = firstName;
+
+        token.transferFrom(address(token), msg.sender, 10000);
     }
 
-    // Add  Disease
+    // Add Disease
     // walletAddress: Address of Patient
     // Rest are details of disease
      function addDisease(address walletAddress, uint8 diseaseID, string memory name, string memory symptoms)
@@ -96,6 +62,7 @@ contract DPRPatient is IERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20V
          patientDisease.Symptoms = symptoms;
          patientDisease.DiseaseID = diseaseID;
          patient.DiseaseCount ++;
+         
      }
 
     // Get Patient details
@@ -132,20 +99,17 @@ contract DPRPatient is IERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20V
         return Diseases[walletAddress][diseaseID];
     }
 
-    // Get the Medicene count of a patient for a disease
+    // Sign Agreement with Doctor to treat a particular disease
     // walletAddress: Address of Patient
     // diseaseID: ID of the disease
-    function getMediceneCount(address walletAddress, uint8 diseaseID) public view returns (uint8)
+    // doctorAddress: Address of Doctor
+    function signAgreementWithDoctor(address walletAddress, uint8 diseaseID, address doctorAddress, IERC20 token) public
     {
-        return Diseases[walletAddress][diseaseID].MediceneCount;
-    }
-
-    // Increment the Medicene count of a patient for a disease
-    // walletAddress: Address of Patient
-    // diseaseID: ID of the disease
-    function updateMediceneCount(address walletAddress, uint8 diseaseID) public returns (uint8)
-    {
-        return Diseases[walletAddress][diseaseID].MediceneCount++;
+         require(msg.sender == walletAddress, "Only owner can add");
+         Agreement storage agreement = Agreements[walletAddress][diseaseID][doctorAddress];
+         
+         token.transferFrom(msg.sender, doctorAddress, 1);
+         agreement.TokenTransferred = 1;
     }
 
     // Get the list of medicenes for a patient.
@@ -169,9 +133,10 @@ contract DPRPatient is IERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20V
     // diseaseID: The Disease ID for which the Medicenes have to be added
     // Rest are details of Medicene
     function prescribeMedicene(address doctorContract, address doctorAddress, address patientAddress, uint8 diseaseID, uint8 mediceneID, string memory name, string memory dose, string memory concentration, uint8 price, uint expiryDate) 
-    public
+    external
     {
-        if (DPRDoctor(doctorContract).isDoctor(doctorAddress) == false)
+        DPRDoctor doctor = DPRDoctor(doctorContract);
+        if (doctor.isDoctor(doctorAddress) == false)
         {
             revert("Only a Dcotor can prescribe Medicene.");
         }
